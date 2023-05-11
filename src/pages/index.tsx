65e9bc6f11a2f16254a2bf5fcf5540142f48ba7b
luke-h1 <luke.howsam@yahoo.com>
2023-05-11 16:45:15 +0100

    init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { useForm, useFieldArray } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <></>;
+};
+export default Home;
